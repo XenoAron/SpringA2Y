0cc8a492375c53ad21dc5746edcf3cb1b530d219
= <jwt1029@midasit.com>
2018-06-05 12:16:14 +0900

    #4. Make HomeController in to package

---
+++ src/main/java/com/xenoaron/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.xenoaron.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @메쏘드명 : HomeController
+ * @작성자 : jwt1029
+ * @작성일자 : 2018-06-05
+ * @설명 :
+ */
+@Controller
+public class HomeController {
+
+}
